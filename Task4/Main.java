c68de1ddd83cdafbcbe87f65c7678befaa4ba391
Денис Пономарев <deskin196@gmail.com>
2023-09-01 12:44:23 +0300

    Написал решение по задаче 4

--- Task4/Main.java
+++ Task4/Main.java
@@ -1,8 +1,19 @@
 //Разработайте программу, которая выбросит Exception, когда пользователь вводит пустую строку.
 //Пользователю должно показаться сообщение, что пустые строки вводить нельзя.
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-       
+       System.out.println(readFromConsole());
+    }
+
+    public static String readFromConsole() throws RaedEmptyStringException {
+        System.out.print("Введите текст: ");
+        try (Scanner input = new Scanner(System.in)) {
+            String result = input.nextLine();
+            if(result.isEmpty()) throw new RaedEmptyStringException();
+            return result;
+        }
     }
 }
---
+++ Task4/RaedEmptyStringException.java
@@ -0,0 +1,3 @@
+public class RaedEmptyStringException extends Exception{
+    
+}
